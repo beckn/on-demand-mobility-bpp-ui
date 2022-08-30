b3b20a8e3b6eae2aa9b5ff6ef5537527aa74428a
Zakir Ansari <zakiransari@Zakirs-MacBook-Pro.local>
2022-08-30 10:16:28 +0530

    change

--- src/components/DriverApp/components/SwitchButton/SwitchButton.js
+++ src/components/DriverApp/components/SwitchButton/SwitchButton.js
@@ -1,22 +1,23 @@
 /* eslint-disable no-undef */
 import React from 'react'
-import './SwitchButton.css'
+import './SwitchButton.css';
 
-const SwitchButton =() => {
-  return (
+
+const SwitchButton= () => {
+  return(
     <div>
-      <div>
-    <h2 className='title'>Bharat Ganapathy</h2>
-    <h3 className='text'>Driver - Beckn One</h3>
-    </div>
+    <div>
+  <h2 className='title'>Bharat Ganapathy</h2>
+  <h3 className='text'>Driver - Beckn One</h3>
+  </div>
 
-     <label className='switch'>
-      <input type='checkbox' />
-      <span className='slider round' />
-      <h3 className='textav'>Available</h3>
-     </label>
-     </div>
+   <label className='switch'>
+    <input type='checkbox' />
+    <span className='slider round' />
+    <h3 className='textav'>Available</h3>
+   </label>
+   </div>
   )
 }
 
-export default SwitchButton
+export default SwitchButton;
