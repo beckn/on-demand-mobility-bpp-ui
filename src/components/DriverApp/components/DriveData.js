f6d824ae64154fdc4135f1ecc794dc96a454faf1
vaishnavi-maker <vaishnavichaudhari009@gmail.com>
2022-09-03 14:58:28 +0530

    fixed Styling issues

---
+++ src/components/DriverApp/components/DriveData.js
@@ -0,0 +1,23 @@
+export const ReadyStartData = {
+    title: 'Ready to Start',
+    Subtitle: 'Please Start the trip to navigate',
+    Location: 'Raja Dinkar Kelkar Museum',
+    Address: 'Shaniwar wada',
+    TotalRide: 'Total Fare',
+    Amount: 'Rs.200',
+    Colletion: 'To be collected at the end of the ride',
+    Ride: 'cancel Ride',
+
+};
+
+export const RideStartedData = {
+    title: 'Ride Started',
+    Location: 'Raja Dinkar Kelkar Museum',
+    subtitle: 'You are on your way towards the drop location',
+};
+
+export const RideCompletedData ={
+    Button: 'Collect Payment',
+    tittle: 'Ride Completed',
+    Subtitle: 'you hav reached te destination'
+}
