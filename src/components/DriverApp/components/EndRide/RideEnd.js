a19e4a764bbb3ea09dc90c4fb3308221ba0d3e0a
Deepak <deepak.sharma@eminds.ai>
2022-09-06 14:08:23 +0530

    Fixing map size issue

--- src/components/DriverApp/components/EndRide/RideEnd.js
+++ src/components/DriverApp/components/EndRide/RideEnd.js
@@ -1,38 +1,32 @@
-import React from 'react'
-import { MapPin } from 'react-feather'
-import { LocationIcon } from '../../../../shared/icons/Location'
-import CustomMap from '../../Maps/CustomMap'
-import { EndRideData } from '../DriveData';
-import './RideEnd.css';
+import React from "react";
+import { MapPin } from "react-feather";
+import { LocationIcon } from "../../../../shared/icons/Location";
+import CustomMap from "../../Maps/CustomMap";
+import { EndRideData } from "../DriveData";
+import "./RideEnd.css";
 
 function RideEnd() {
   return (
     <div>
-    <h1 className='titleP'>{EndRideData.title}</h1>
-    <p className='subP'>{EndRideData.Subtitle}</p>
-    <h2 className='Rp'>{EndRideData.Rupues}</h2>
-    <hr className='hrp'/>
-    <h6 className='date'>{EndRideData.Date}</h6>
-    <h6 className='dist'>{EndRideData.Distance}</h6>
-    <h6 className='tm'>{EndRideData.Time}</h6>
-    <div className='locationI'>
-    <LocationIcon />
-   <p className='LP'>
-   {EndRideData.addres}
-   </p> 
-    <div className="MaP">
-        <MapPin color="#D22323" />
-      <p className='Mpin'>{EndRideData.LocationPin}</p>  
+      <h1 className="titleP">{EndRideData.title}</h1>
+      <p className="subP">{EndRideData.Subtitle}</p>
+      <h2 className="Rp">{EndRideData.Rupues}</h2>
+      <hr className="hrp" />
+      <h6 className="date">{EndRideData.Date}</h6>
+      <h6 className="dist">{EndRideData.Distance}</h6>
+      <h6 className="tm">{EndRideData.Time}</h6>
+      <div className="locationI">
+        <LocationIcon />
+        <p className="LP">{EndRideData.addres}</p>
+        <div className="MaP">
+          <MapPin color="#D22323" />
+          <p className="Mpin">{EndRideData.LocationPin}</p>
+        </div>
       </div>
-      </div>
-      <CustomMap />
-      <button className="End fixed-bottom">
-        End Ride
-    </button>
+      <CustomMap customHeight={"450px"} />
+      <button className="End fixed-bottom">End Ride</button>
     </div>
-    
-
-  )
+  );
 }
 
-export default RideEnd
+export default RideEnd;
