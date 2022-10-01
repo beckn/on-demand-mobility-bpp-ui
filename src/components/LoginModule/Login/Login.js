91008a3d7f70003de73fa39cbb916d5bf301ecb1
Deepak <deepak.sharma@eminds.ai>
2022-10-01 20:02:05 +0530

    Name fix

--- src/components/LoginModule/Login/Login.js
+++ src/components/LoginModule/Login/Login.js
@@ -38,7 +38,7 @@ export const Login = (props) => {
               <div className="row w-100 justify-content-center">
                 <div className="col-12 col-lg-6">
                   <Title>
-                    Welcome <br /> to the {App} App
+                    Welcome <br /> to the Neo Rides
                   </Title>
                   <ButtonsContainer>
                     <Link
