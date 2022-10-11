2933c7e8406ec19fedf11636ca187c4badcad3de
Deepak <deepak.sharma@eminds.ai>
2022-10-11 19:07:26 +0530

    Fixing api key issue

--- src/components/LoginModule/Login.services.js
+++ src/components/LoginModule/Login.services.js
@@ -12,11 +12,12 @@ export const getRoles = (path, fields) => {
 
 export const userAction = async (path, data, fields) => {
   const logRes = await postRequestData(path, data, fields);
+  setApiKey(logRes.data.User);
+
   let userUrl = `users/show/${logRes.data.User.Id}`;
   let fieldset = `{"User":["Name","DateOfBirth","Id","AddressLine1","AddressLine2","AddressLine3","CityId","FirstName","LastName","PhoneNumber","PinCodeId","Verified"],"DriverDocument":["Id","Document","ImageUrl","DocumentNumber","Verified"]}`;
   const getUser = await getRequestData(userUrl, UserFields);
   // console.log("userAction", getUser, logRes);
-  setApiKey(logRes.data.User);
   setUser(getUser.data.User);
   if (data.User.UserRole) {
     let roleUrl = "user_roles/save";
